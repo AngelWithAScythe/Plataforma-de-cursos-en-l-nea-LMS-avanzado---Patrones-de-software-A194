cf0c493c791b6ccc272fc4ab4822b39f42545ee6
Kayden <juanscardenas2003@gmail.com>
2026-09-06 17:05:36 -0500

    Correción semana 3

---
+++ codigo_semana_3/src/main/java/com/java/lms/ProyectoLmsPdsApplication.java
@@ -0,0 +1,13 @@
+package com.java.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLmsPdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLmsPdsApplication.class, args);
+	}
+
+}
